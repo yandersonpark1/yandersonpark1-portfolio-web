909465e335436e71965e0111a0a5f14bf68d0f85
yandersonpark1 <everydayap3@gmail.com>
2025-09-01 17:44:16 -0400

    inital page and update to include about, header, hero

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+    </div>
+  )
+}
