10b9ebaaf0a275b018c75c4f912e559bdf3c19a3
yandersonpark1 <everydayap3@gmail.com>
2025-09-01 18:39:07 -0400

    reset tailwind to version 3 for easier use // configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'gradient-x': 'gradient-x 15s ease infinite',
-        'float-1': 'float-1 6s ease-in-out infinite',
-        'float-2': 'float-2 8s ease-in-out infinite',
-        'float-3': 'float-3 10s ease-in-out infinite',
-      },
-      keyframes: {
-        'gradient-x': {
-          '0%, 100%': {
-            'background-size': '200% 200%',
-            'background-position': 'left center'
-          },
-          '50%': {
-            'background-size': '200% 200%',
-            'background-position': 'right center'
-          },
-        },
-        'float-1': {
-          '0%, 100%': { transform: 'translateY(0px) rotate(0deg)' },
-          '50%': { transform: 'translateY(-20px) rotate(180deg)' },
-        },
-        'float-2': {
-          '0%, 100%': { transform: 'translateY(0px) rotate(0deg)' },
-          '50%': { transform: 'translateY(-30px) rotate(-180deg)' },
-        },
-        'float-3': {
-          '0%, 100%': { transform: 'translateY(0px) rotate(0deg)' },
-          '50%': { transform: 'translateY(-40px) rotate(360deg)' },
-        },
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
