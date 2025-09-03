34a4e2475388f45483da5fbd4781ad1435c166c9
yandersonpark1 <everydayap3@gmail.com>
2025-09-03 11:15:48 -0400

    about fix

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -32,6 +32,7 @@ export default function About() {
     },
   ]
 
+
   return (
     <section id="about" className="py-20 bg-gray-50">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
